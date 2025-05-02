fc240cdb0208c1031e041439dd3e962d17e6e104
Abdul Rafay  CSC Student <f22csc001@shu.edu.pk>
2025-05-03 02:20:43 +0500

    corrected the route

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return;
-  <div className="flex justify-center items-center h-screen">
-    <SignUp />
-  </div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignUp />
+    </div>
+  );
 }
